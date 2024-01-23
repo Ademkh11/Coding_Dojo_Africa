ff92ac0c860b5366202b35743bd552a2c37eef5c
Ademkh11 <156675564+Ademkh11@users.noreply.github.com>
2024-01-23 22:12:06 +0100

    Add files via upload

---
+++ Khlifa_Adem_JS_CreateVariables.js
@@ -0,0 +1,8 @@
+var minimum_age = 10;
+var minimum_height = 42;
+if(minimum_height=>42,minimum_age=>10){
+    console.log("Get on that ride,kiddo!");
+}
+else{
+    console.log("Sorry kiddo.Maybe next year.");
+}
