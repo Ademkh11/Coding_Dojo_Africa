eabaef1e44941b9973a287070a9a0d41e61a9e10
Ademkh11 <156675564+Ademkh11@users.noreply.github.com>
2024-01-27 17:32:56 +0100

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,7 @@
+// YOUR COMMENT HERE - what would this variable do? this variable named Likecount will allow us to declare the value that's assigned to it 
+var likeCount = 0;
+// YOUR COMMENT HERE - what would be the function of this code block? the function of this code block will be : increseing the number of likes 
+function increaseLikes() {
+    // YOUR COMMENT HERE - what is this sort operation for? this sort of operation is used for addition
+    likeCount = likeCount + 1;
+}
